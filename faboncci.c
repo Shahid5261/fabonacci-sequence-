ba1585aa6e28bd26d9bdd83ad83e7216cac83a75
Shahid5261 <90671128+Shahid5261@users.noreply.github.com>
2021-12-15 05:58:08 +0530

    Add files via upload

---
+++ faboncci.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int n,f;
+	printf("enter any number");
+	scanf("%d",&n);
+	f=fab(n);
+	printf("%d sequence of fabonacci is %d",n,f);
+	
+}
+int fab(int n)
+{
+	if(n==1||n==2)
+	{
+		return 1;
+		
+	}
+	else
+	{
+		return fab(n-1)+fab(n-2);
+	}
+
+}
